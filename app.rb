6a34c2ce7130a3598b2e620e3dfc5fdecf313c46
Baoshan Sheng <sheng@icmd.org>
2012-04-20 17:53:55 +0800

    App.rb

---
+++ app.rb
@@ -0,0 +1,40 @@
+require 'rubygems'
+require 'bundler/setup'
+require 'linguist'
+require 'sinatra'
+
+get '/stat/*' do
+  repo = Linguist::Repository.from_directory("/#{params[:splat]}")
+  stream do |out|
+    repo.languages.sort_by { |_, size| size }.reverse.each do |language, size|
+      percentage = ((size / repo.size.to_f) * 100).round
+      out << "%-4s %-5s %s\n" % ["#{percentage}%", size, language]
+    end
+  end
+end
+
+get '/touched' do
+  dir  = '/' + params['path']
+  file = File.open dir + '/.created'
+  stream do |out|
+    file.each do |source|
+        source = source.strip
+        blob = Linguist::FileBlob.new dir + '/' + source
+	next if blob.vendored? || blob.generated? || blob.language.nil?
+        if blob.extname.empty?
+	  out << (source + blob.language.primary_extension + '_' + "\n")
+	else
+	  dirname  = File.dirname  source
+	  if dirname != '.' 
+	    dirname += '/'
+	  else
+	    dirname = ''
+	  end
+	  extname  = File.extname  source
+	  basename = File.basename source, extname
+	  extname  = extname.sub '_', '__'
+	  out << (dirname + basename + extname + "\n")
+	end
+    end
+  end
+end
