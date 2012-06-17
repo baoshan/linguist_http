2c013a91b1985b87e25fa527c4df77966f8528c5
Baoshan Sheng <sheng@icmd.org>
2012-06-17 05:35:59 -0400

    Improve Indention

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ get '/statist' do
   stream do |out|
     repo.languages.sort_by { |_, size| size }.reverse.each do |language, size|
       percentage = ((size / repo.size.to_f) * 100).round
-      out << "%s %s %s\n" % ["#{percentage}%", size, language]
+      out << "{%s}{%s}{%s}\n" % ["#{percentage}%", size, language]
     end
   end
 end
@@ -18,23 +18,23 @@ get '/sources' do
   file = File.open params['list']
   stream do |out|
     file.each do |source|
-        source = source.strip
-        blob = Linguist::FileBlob.new path + '/' + source
-	next if blob.vendored? || blob.generated? || blob.language.nil?
-        if blob.extname.empty?
-	  out << (source + blob.language.primary_extension + '_' + "\n")
-	else
-	  dirname  = File.dirname source
-	  if dirname != '.' 
-	    dirname += '/'
-	  else
-	    dirname = ''
-	  end
-	  extname  = File.extname source
-	  basename = File.basename source, extname
-	  extname  = extname.sub '_', '__'
-	  out << (dirname + basename + extname + "\n")
-	end
+      source = source.strip
+      blob = Linguist::FileBlob.new path + '/' + source
+      next if blob.vendored? || blob.generated? || blob.language.nil?
+      if blob.extname.empty?
+        out << (source + blob.language.primary_extension + '_' + "\n")
+      else
+        dirname  = File.dirname source
+        if dirname != '.' 
+          dirname += '/'
+        else
+          dirname = ''
+        end
+        extname  = File.extname source
+        basename = File.basename source, extname
+        extname  = extname.sub '_', '__'
+        out << (dirname + basename + extname + "\n")
+      end
     end
   end
 end
